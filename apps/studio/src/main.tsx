2e34cf09c8ac68d2680dba483f817ac8abb6a7f6
Amaan Syed <amaansyed27@gmail.com>
2026-06-22 20:22:19 +0530

    Load engine cleanup styles

--- apps/studio/src/main.tsx
+++ apps/studio/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./preview3d.css";
+import "./engine-cleanup.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
