4b1803045ea0cade93ee0cc66f66bbb36496b1cb
Amaan Syed <amaansyed27@gmail.com>
2026-06-22 21:19:10 +0530

    Install modular CSS preview runtime

--- apps/studio/src/main.tsx
+++ apps/studio/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./preview3d.css";
 import "./engine-cleanup.css";
+import { installCssPreviewRuntime } from "./features/preview/cssPreviewRuntime";
+
+installCssPreviewRuntime();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
