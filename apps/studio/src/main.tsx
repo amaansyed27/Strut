f03c9380c461117d0d989645b194852f5da8043e
Amaan Syed <amaansyed27@gmail.com>
2026-06-22 17:24:00 +0530

    Load preview perspective CSS

--- apps/studio/src/main.tsx
+++ apps/studio/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./preview3d.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
